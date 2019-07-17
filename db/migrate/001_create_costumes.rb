01713e79ec419a064b3f7e1ab9817c0826258f09
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-17 23:49:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,15 @@
 class CreateCostumes < ActiveRecord::Migration
   
+    def change
+       create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+  
 end
   
 
