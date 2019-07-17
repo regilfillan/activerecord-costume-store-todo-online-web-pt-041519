31a99d64874629ab804b376a28ff643abf703364
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-17 23:47:18 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -4,7 +4,13 @@
 class Costume < ActiveRecord::Base 
   
   def change
-    
+       create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
   end
   
 end
