ab7152d437a1b27decf6faaf84829251c3a95069
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-18 00:00:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,6 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration
         t.datetime :opening_time
         t.datetime :closing_time
         t.timestamps null: false
+      end
     end
   end
   
